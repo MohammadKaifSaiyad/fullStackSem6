a9078949bdc8ac0d2b745bcd6f981502313ffdd9
MohammadKaifSaiyad <saiyedkgn6@gmail.com>
2024-05-03 09:14:54 +0530

    added

--- frontend/src/components/AddItem.js
+++ frontend/src/components/AddItem.js
@@ -208,7 +208,7 @@ function AddItem({itemData, setItemData, selectedArea, fetchItemsByArea, setSele
               value={itemData.item_name}
               onChange={handleChange}
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Enter area name"
+              placeholder="Enter item name"
               required
             />
           </div>
